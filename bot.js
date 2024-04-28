77ff664d4ee3a126e3b5b3dd08a2fbe60f4a289d
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-04-28 09:13:50 +0400

    feat: remove typescript
    also add create route to the app

--- bot.js
+++ bot.js
@@ -1,21 +1,58 @@
 import * as dotenv from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
 dotenv.config();
+
 const token = process.env.BOT_TOKEN;
 const appURL = process.env.APP_URL;
+
 if (!token || !appURL) {
-    throw Error('token or app url not found');
+  throw Error('token or app url not found');
 }
+
 const bot = new TelegramBot(token, { polling: true });
+
 bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-    const options = {
-        reply_markup: {
-            inline_keyboard: [[{ text: 'Courses', web_app: { url: appURL } }]],
-        },
-    };
-    if (text === '/start') {
-        await bot.sendMessage(chatId, 'Click the button below if you want to buy or create your own online course', options);
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const getOptions = (type) => {
+    let url;
+    let text;
+    switch (type) {
+      case 'create':
+        url = `${appURL}create`;
+        text = 'Create course';
+        console.log('url', url);
+        break;
+      default:
+      case 'start':
+        url = appURL;
+        text = 'View courses';
+        break;
     }
+
+    return {
+      reply_markup: {
+        inline_keyboard: [[{ text, web_app: { url } }]],
+      },
+    };
+  };
+
+  switch (text) {
+    case '/create':
+      await bot.sendMessage(
+        chatId,
+        'Click the button below if you want to create your own online course',
+        getOptions('create')
+      );
+      break;
+
+    case '/start':
+    default:
+      await bot.sendMessage(
+        chatId,
+        'Click the button below if you want to buy online course',
+        getOptions('start')
+      );
+      break;
+  }
 });
