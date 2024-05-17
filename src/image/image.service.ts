28ed60b296e0571d60683bf7a8b8a276a2c9eed7
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-05-17 16:54:22 +0400

    feat: recover image service for uploading new image to the Cloudinary
    also add lessons to the course response;

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -11,51 +11,46 @@ export class ImageService {
     private readonly logger: MyLogger,
   ) {}
 
-  async upload(temporaryImageUrl: string, courseId: string) {
-    console.log('temporaryImageUrl', temporaryImageUrl);
-    console.log('courseId', courseId);
-    //   // Try to get the current image for the course
-    //   const currentImage = await this.prisma.image.findUnique({
-    //     where: {
-    //       type: 'Course',
-    //       typeId: courseId,
-    //     },
-    //   });
-    //   // Delete the old image from Cloudinary
-    //   if (currentImage && currentImage.publicId) {
-    //     try {
-    //       await this.cloudinaryService.deleteFile(currentImage.publicId);
-    //     } catch (error) {
-    //       this.logger.error({
-    //         method: 'image-delete-cloudinary',
-    //         error,
-    //       });
-    //     }
-    //   }
-    //   // Upload a new image to Cloudinary
-    //   let imageFromCloudinary;
-    //   try {
-    //     imageFromCloudinary =
-    //       await this.cloudinaryService.uploadImageUrl(temporaryImageUrl);
-    //   } catch (error) {
-    //     this.logger.error({ method: 'image-upload-cloudinary', error });
-    //     throw new Error('Failed to upload new image');
-    //   }
-    //   return await this.prisma.image.upsert({
-    //     where: {
-    //       type: 'Course',
-    //       typeId: courseId,
-    //     },
-    //     create: {
-    //       type: 'Course',
-    //       typeId: courseId,
-    //       url: imageFromCloudinary?.url,
-    //       publicId: imageFromCloudinary?.public_id,
-    //     },
-    //     update: {
-    //       url: imageFromCloudinary?.url,
-    //       publicId: imageFromCloudinary?.public_id,
-    //     },
-    //   });
+  async upload(temporaryImageUrl: string, courseId: string, userId: number) {
+    // Try to get the current image for the course
+    const course = await this.prisma.course.findFirst({
+      where: {
+        userId,
+        id: courseId,
+      },
+    });
+
+    // Delete the old image from Cloudinary
+    if (course && course.imageUrl && course.imagePublicId) {
+      try {
+        await this.cloudinaryService.deleteFile(course.imagePublicId);
+      } catch (error) {
+        this.logger.error({
+          method: 'image-delete-cloudinary',
+          error,
+        });
+      }
+    }
+
+    // Upload a new image to Cloudinary
+    let imageFromCloudinary;
+    try {
+      imageFromCloudinary =
+        await this.cloudinaryService.uploadImageUrl(temporaryImageUrl);
+    } catch (error) {
+      this.logger.error({ method: 'image-upload-cloudinary', error });
+      throw new Error('Failed to upload new image');
+    }
+
+    return await this.prisma.course.update({
+      where: {
+        userId,
+        id: courseId,
+      },
+      data: {
+        imageUrl: imageFromCloudinary?.url,
+        imagePublicId: imageFromCloudinary?.public_id,
+      },
+    });
   }
 }
