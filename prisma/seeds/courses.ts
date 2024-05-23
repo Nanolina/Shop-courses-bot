c7b6df0167b8a099157f3e1395caf10a97adce43
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-05-23 12:02:15 +0400

    feat: add our courses
    add Sneganna user and Alina user with courses, modules, lessons to seed

--- prisma/seeds/courses.ts
+++ prisma/seeds/courses.ts
@@ -5,11 +5,11 @@ const prisma = new PrismaClient();
 async function main() {
   console.log('Starting to seed...');
 
-  // Creating a User
-  const user = await prisma.user.create({
+  // Creating a User Alina
+  const userAlina = await prisma.user.create({
     data: {
       id: 'ca5df41b-f2b0-4442-9c0b-943e2732fa02',
-      tgId: 3458,
+      tgId: 1708576552,
       courses: {
         create: [
           {
@@ -40,6 +40,13 @@ async function main() {
                           'https://docs.tact-lang.org/_next/static/media/banner.0c18b672.jpeg',
                         videoUrl: 'https://www.youtube.com/watch?v=a3Jyy-z4Kmo',
                       },
+                      {
+                        name: 'Lesson 2',
+                        description: 'First smart-contracts',
+                        imageUrl:
+                          'https://media.geeksforgeeks.org/wp-content/uploads/20220527093234/Howdoesasmartcontractwork1.jpg',
+                        videoUrl: 'https://youtu.be/vZnR-pCiGNQ',
+                      },
                     ],
                   },
                 },
@@ -51,7 +58,61 @@ async function main() {
     },
   });
 
-  console.log(`Created user with ID: ${user.id}`);
+  // Creating a User Sneganna
+  const userSneganna = await prisma.user.create({
+    data: {
+      id: 'b63defae-f9e9-4e0f-b4f1-1a789c6e2617',
+      tgId: 5075565141,
+      courses: {
+        create: [
+          {
+            name: 'React',
+            description:
+              'React Полный курс от А до Я. Рассмотрим основные концепции и разработаем функционал, который встречается в каждом приложении.',
+            category: 'Technology',
+            subcategory: 'Full-stack development',
+            price: 12,
+            currency: 'TON',
+            imageUrl:
+              'https://avatars.mds.yandex.net/i?id=26246968997c93bde28e69b7017205039296dbdc-10393597-images-thumbs&n=13',
+            walletAddressSeller:
+              '0:16ee206614c0d755460d0728d875c6ce46065c138b1d2ce5d1524b5f74715d61',
+            modules: {
+              create: [
+                {
+                  name: 'Теория',
+                  description: 'Рассмотрим ключевые моменты',
+                  imageUrl:
+                    'https://avatars.mds.yandex.net/i?id=11f761e7e525de063283c1945737b93a-4504275-images-thumbs&n=13',
+                  lessons: {
+                    create: [
+                      {
+                        name: 'Создание проекта',
+                        description:
+                          'Научимся создавать первый React проект и изучим всю структуру файлов',
+                        videoUrl:
+                          'https://youtu.be/GNrdg3PzpJQ?list=PL6DxKON1uLOHya4bDIynPTCwZHrezUlFs',
+                      },
+                      {
+                        name: 'Компоненты',
+                        description: 'Напишем первые компоненты на React',
+                        imageUrl:
+                          'https://www.pvsm.ru/images/2018/11/13/vvedenie-v-React%C2%A0Hooks-17.png',
+                        videoUrl: 'https://youtu.be/H2GCkRF9eko',
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  console.log(`Created user with ID: ${userAlina.id}`);
+  console.log(`Created user with ID: ${userSneganna.id}`);
 }
 
 main()
