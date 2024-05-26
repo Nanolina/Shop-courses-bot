694d6e38f6fbcdb95039f315bbf4d6dec115cae8
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-05-26 09:12:16 +0300

    feat: add handlers to the Telegram module

--- src/telegram-bot/telegram.module.ts
+++ src/telegram-bot/telegram.module.ts
@@ -6,6 +6,13 @@ import { CourseModule } from '../course/course.module';
 import { CourseService } from '../course/course.service';
 import { LoggerModule } from '../logger/logger.module';
 import { PrismaService } from '../prisma/prisma.service';
+import {
+  CallbackQueryHandler,
+  CourseHandlers,
+  LessonHandlers,
+  ModuleHandlers,
+  TextCommandHandler,
+} from './handlers';
 import { TelegramBotService } from './telegram-bot.service';
 import { TelegramListenersService } from './telegram-listeners.service';
 import { TelegramUtilsService } from './telegram-utils.service';
@@ -20,6 +27,11 @@ import { TelegramUtilsService } from './telegram-utils.service';
     CourseService,
     ModuleService,
     LessonService,
+    CourseHandlers,
+    ModuleHandlers,
+    LessonHandlers,
+    TextCommandHandler,
+    CallbackQueryHandler,
   ],
   exports: [TelegramBotService],
 })
