6f8beb9c5f0e8ec19f3d26519a6bc75e06a19467
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-06-24 11:41:52 +0400

    feat: change courses seed to use prod and dev imageUrl and videoUrl

---
+++ prisma/seeds/coursesDataAlina.ts
@@ -0,0 +1,425 @@
+export const coursesDataAlinaDev = [
+  {
+    name: 'Meditation',
+    description:
+      'Guided courses to help you achieve mindfulness and relaxation.',
+    category: 'personal-development',
+    subcategory: 'yoga-mindfulness-and-meditation',
+    price: 12,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718612312/course/Screenshot_2024-06-17_at_12.17.53_rkxonh.png',
+    imagePublicId: 'course/Screenshot_2024-06-17_at_12.17.53_rkxonh',
+    modules: [
+      {
+        name: 'Basics of Meditation',
+        description: 'Understand the fundamental techniques of meditation.',
+        lessons: [
+          {
+            name: 'Breathing Techniques',
+            description:
+              'Learn how to control your breath and use it to foster calmness.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718612367/lesson/huermkicjsvm2vlpac1l.mp4',
+            videoPublicId: 'lesson/huermkicjsvm2vlpac1l',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Dance Training',
+    description: 'Step-by-step dance lessons to get you moving to the rhythm.',
+    category: 'lifestyle-and-entertainment',
+    subcategory: 'music-and-dance',
+    price: 18,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718613014/course/Screenshot_2024-06-17_at_12.29.47_ziavve.png',
+    imagePublicId: 'course/Screenshot_2024-06-17_at_12.29.47_ziavve',
+    modules: [
+      {
+        name: 'Beginner Steps',
+        description:
+          'Start your dance journey with simple, foundational steps.',
+        lessons: [
+          {
+            name: 'Basic Footwork',
+            description: 'Learn the basic footwork of popular dance styles.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718613172/lesson/x9cejjegc0v5dk9krtlw.mp4',
+            videoPublicId: 'lesson/x9cejjegc0v5dk9krtlw',
+          },
+          {
+            name: 'Hands',
+            description: 'Learn the basic handswork of popular dance styles.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718613281/lesson/t0dvnhag2k3wbdfrrrox.mp4',
+            videoPublicId: 'lesson/t0dvnhag2k3wbdfrrrox',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Yoga',
+    description:
+      'Yoga courses designed to improve flexibility and mental health.',
+    category: 'personal-development',
+    subcategory: 'yoga-mindfulness-and-meditation',
+    price: 15,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718613360/course/Screenshot_2024-06-17_at_12.35.26_fnncbe.png',
+    imagePublicId: 'course/Screenshot_2024-06-17_at_12.35.26_fnncbe',
+    modules: [
+      {
+        name: 'Yoga Foundations',
+        description: 'Fundamental yoga poses and sequences for beginners.',
+        lessons: [
+          {
+            name: 'Yoga Poses for Beginners',
+            description:
+              'Learn the core yoga poses and how to perform them safely.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718613547/lesson/gud7e5n6uwqx6cyaedhq.mp4',
+            videoPublicId: 'lesson/gud7e5n6uwqx6cyaedhq',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Product Photography',
+    description:
+      'Learn the techniques for taking professional quality product photos.',
+    category: 'arts-design-and-media',
+    subcategory: 'photography',
+    price: 22,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718613813/course/Screenshot_2024-06-17_at_12.43.03_fuivco.png',
+    imagePublicId: 'course/Screenshot_2024-06-17_at_12.43.03_fuivco',
+    modules: [
+      {
+        name: 'Jewelry Photography',
+        description:
+          'Learn the specific techniques required for photographing jewelry with precision and clarity.',
+        lessons: [
+          {
+            name: 'Composition Techniques',
+            description:
+              'Explore how to compose shots that focus on the elegance and details of jewelry pieces.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718613692/lesson/dievnnfwsemp5gurrdnn.mp4',
+            videoPublicId: 'lesson/dievnnfwsemp5gurrdnn',
+          },
+        ],
+      },
+      {
+        name: 'Cake Photography',
+        description:
+          'Master the art of capturing stunning photos of cakes for bakeries and events.',
+        lessons: [
+          {
+            name: 'Lighting and Composition for Cakes',
+            description:
+              'Learn the best lighting setups and compositional techniques to make cakes look appealing in photos.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718613603/lesson/jdwtnxhycu4lswgrvvy9.mp4',
+            videoPublicId: 'lesson/jdwtnxhycu4lswgrvvy9',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Sponge Cake Baking with Crème Brûlée Icing',
+    description:
+      'Learn to bake a classic sponge cake topped with delicious crème brûlée icing from scratch.',
+    category: 'lifestyle-and-entertainment',
+    subcategory: 'cooking-and-baking',
+    price: 20,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718615669/course/pexels-zvolskiy-1721932_l0fuve.jpg',
+    imagePublicId: 'course/pexels-zvolskiy-1721932_l0fuve',
+    modules: [
+      {
+        name: 'Baking the Sponge Cake with cream',
+        description:
+          'Master the art of baking the perfect sponge cake with step-by-step instructions.',
+        lessons: [
+          {
+            name: 'Preparing the Batter',
+            description:
+              'Learn how to mix the batter to achieve a light and fluffy sponge.',
+            imageUrl:
+              'https://res.cloudinary.com/dbrquscbv/image/upload/v1718615463/course/Screenshot_2024-06-17_at_13.05.49_justvk.png',
+            imagePublicId: 'course/Screenshot_2024-06-17_at_13.05.49_justvk',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718615770/lesson/veawrugaxhmye2ttkpyu.mp4',
+            videoPublicId: 'lesson/veawrugaxhmye2ttkpyu',
+          },
+        ],
+      },
+      {
+        name: 'Assembling the cake',
+        description: 'Learn how to properly assemble a sponge cake',
+        imageUrl:
+          'https://res.cloudinary.com/dbrquscbv/image/upload/v1718615463/course/Screenshot_2024-06-17_at_13.06.38_hwaxid.png',
+        imagePublicId: 'course/Screenshot_2024-06-17_at_13.06.38_hwaxid',
+        lessons: [
+          {
+            name: 'Put the filling on the biscuit',
+            description:
+              'Learn how to spread the cream so that the cake does not fall apart and is the same size on all sides.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718615827/lesson/bj5ngbax2m5r3ipamiee.mp4',
+            videoPublicId: 'lesson/bj5ngbax2m5r3ipamiee',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Automotive Tech: Electronics & Mechanics',
+    description:
+      'Dive into the comprehensive study of automotive technology with a dual focus on electronics and mechanics. This course offers in-depth knowledge on the diagnostics, and repair of automotive systems.',
+    category: 'engineering',
+    subcategory: 'mechanical-engineering',
+    price: 15,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718791722/course/Screenshot_2024-06-19_at_14.08.02_aequij.png',
+    imagePublicId: 'course/Screenshot_2024-06-19_at_14.08.02_aequij',
+    modules: [
+      {
+        name: 'Mechanic',
+        description: '',
+        lessons: [
+          {
+            name: 'First lesson',
+            description: '',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718791486/lesson/gptzjfqazondt1tjetw7.mp4',
+            videoPublicId: 'lesson/gptzjfqazondt1tjetw7',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const coursesDataAlinaProd = [
+  {
+    name: 'Meditation',
+    description:
+      'Guided courses to help you achieve mindfulness and relaxation.',
+    category: 'personal-development',
+    subcategory: 'yoga-mindfulness-and-meditation',
+    price: 12,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212245/Screenshot_2024-06-17_at_12.17.53_rkxonh_wftifa.png',
+    imagePublicId: 'Screenshot_2024-06-17_at_12.17.53_rkxonh_wftifa',
+    modules: [
+      {
+        name: 'Basics of Meditation',
+        description: 'Understand the fundamental techniques of meditation.',
+        lessons: [
+          {
+            name: 'Breathing Techniques',
+            description:
+              'Learn how to control your breath and use it to foster calmness.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212362/huermkicjsvm2vlpac1l_n5p0mn.mp4',
+            videoPublicId: 'huermkicjsvm2vlpac1l_n5p0mn',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Dance Training',
+    description: 'Step-by-step dance lessons to get you moving to the rhythm.',
+    category: 'lifestyle-and-entertainment',
+    subcategory: 'music-and-dance',
+    price: 18,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212242/pexels-polina-tankilevitch-5385707_f7iguu_gtn4s0.jpg',
+    imagePublicId: 'pexels-polina-tankilevitch-5385707_f7iguu_gtn4s0',
+    modules: [
+      {
+        name: 'Beginner Steps',
+        description:
+          'Start your dance journey with simple, foundational steps.',
+        lessons: [
+          {
+            name: 'Basic Footwork',
+            description: 'Learn the basic footwork of popular dance styles.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212348/zzvy1qphb5g7ne6r9mpu_nqihrs.mp4',
+            videoPublicId: 'zzvy1qphb5g7ne6r9mpu_nqihrs',
+          },
+          {
+            name: 'Hands',
+            description: 'Learn the basic handswork of popular dance styles.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212347/tovfelo53yfbv6rfcywp_z6st7o.mp4',
+            videoPublicId: 'tovfelo53yfbv6rfcywp_z6st7o',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Yoga',
+    description:
+      'Yoga courses designed to improve flexibility and mental health.',
+    category: 'personal-development',
+    subcategory: 'yoga-mindfulness-and-meditation',
+    price: 15,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212255/Screenshot_2024-06-17_at_12.35.26_fnncbe_fwwefd.png',
+    imagePublicId: 'Screenshot_2024-06-17_at_12.35.26_fnncbe_fwwefd',
+    modules: [
+      {
+        name: 'Yoga Foundations',
+        description: 'Fundamental yoga poses and sequences for beginners.',
+        lessons: [
+          {
+            name: 'Yoga Poses for Beginners',
+            description:
+              'Learn the core yoga poses and how to perform them safely.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212392/gud7e5n6uwqx6cyaedhq_rtyk5o.mp4',
+            videoPublicId: 'gud7e5n6uwqx6cyaedhq_rtyk5o',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Product Photography',
+    description:
+      'Learn the techniques for taking professional quality product photos.',
+    category: 'arts-design-and-media',
+    subcategory: 'photography',
+    price: 22,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212246/Screenshot_2024-06-17_at_12.43.03_fuivco_scar8x.png',
+    imagePublicId: 'Screenshot_2024-06-17_at_12.43.03_fuivco_scar8x',
+    modules: [
+      {
+        name: 'Jewelry Photography',
+        description:
+          'Learn the specific techniques required for photographing jewelry with precision and clarity.',
+        lessons: [
+          {
+            name: 'Composition Techniques',
+            description:
+              'Explore how to compose shots that focus on the elegance and details of jewelry pieces.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212401/dievnnfwsemp5gurrdnn_dxxhfh.mp4',
+            videoPublicId: 'dievnnfwsemp5gurrdnn_dxxhfh',
+          },
+        ],
+      },
+      {
+        name: 'Cake Photography',
+        description:
+          'Master the art of capturing stunning photos of cakes for bakeries and events.',
+        lessons: [
+          {
+            name: 'Lighting and Composition for Cakes',
+            description:
+              'Learn the best lighting setups and compositional techniques to make cakes look appealing in photos.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212370/jdwtnxhycu4lswgrvvy9_h5ij2h.mp4',
+            videoPublicId: 'jdwtnxhycu4lswgrvvy9_h5ij2h',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Sponge Cake Baking with Crème Brûlée Icing',
+    description:
+      'Learn to bake a classic sponge cake topped with delicious crème brûlée icing from scratch.',
+    category: 'lifestyle-and-entertainment',
+    subcategory: 'cooking-and-baking',
+    price: 20,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212243/pexels-zvolskiy-1721932_l0fuve_ytrplq.jpg',
+    imagePublicId: 'pexels-zvolskiy-1721932_l0fuve_ytrplq',
+    modules: [
+      {
+        name: 'Baking the Sponge Cake with cream',
+        description:
+          'Master the art of baking the perfect sponge cake with step-by-step instructions.',
+        lessons: [
+          {
+            name: 'Preparing the Batter',
+            description:
+              'Learn how to mix the batter to achieve a light and fluffy sponge.',
+            imageUrl:
+              'https://res.cloudinary.com/dbrquscbv/image/upload/v1718615463/course/Screenshot_2024-06-17_at_13.05.49_justvk.png',
+            imagePublicId: 'course/Screenshot_2024-06-17_at_13.05.49_justvk',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212349/veawrugaxhmye2ttkpyu_s3mlev.mp4',
+            videoPublicId: 'veawrugaxhmye2ttkpyu_s3mlev',
+          },
+        ],
+      },
+      {
+        name: 'Assembling the cake',
+        description: 'Learn how to properly assemble a sponge cake',
+        imageUrl:
+          'https://res.cloudinary.com/dbrquscbv/image/upload/v1718615463/course/Screenshot_2024-06-17_at_13.06.38_hwaxid.png',
+        imagePublicId: 'course/Screenshot_2024-06-17_at_13.06.38_hwaxid',
+        lessons: [
+          {
+            name: 'Put the filling on the biscuit',
+            description:
+              'Learn how to spread the cream so that the cake does not fall apart and is the same size on all sides.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212382/bj5ngbax2m5r3ipamiee_ktdjql.mp4',
+            videoPublicId: 'bj5ngbax2m5r3ipamiee_ktdjql',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Automotive Tech: Electronics & Mechanics',
+    description:
+      'Dive into the comprehensive study of automotive technology with a dual focus on electronics and mechanics. This course offers in-depth knowledge on the diagnostics, and repair of automotive systems.',
+    category: 'engineering',
+    subcategory: 'mechanical-engineering',
+    price: 15,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212260/Screenshot_2024-06-19_at_14.08.02_aequij_irojrw.png',
+    imagePublicId: 'Screenshot_2024-06-19_at_14.08.02_aequij_irojrw',
+    modules: [
+      {
+        name: 'Mechanic',
+        description: '',
+        lessons: [
+          {
+            name: 'First lesson',
+            description: '',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212409/gptzjfqazondt1tjetw7_mohvwb.mp4',
+            videoPublicId: 'lesson/gptzjfqazondt1tjetw7_mohvwb',
+          },
+        ],
+      },
+    ],
+  },
+];
---
+++ prisma/seeds/coursesSnezhanna.ts
@@ -0,0 +1,251 @@
+export const coursedDataSnezhannaDev = [
+  {
+    name: 'Videography',
+    description:
+      'Courses on shooting and editing video to create stunning visual content.',
+    category: 'arts-design-and-media',
+    subcategory: 'video-production-and-editing',
+    price: 25,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718614671/course/Screenshot_2024-06-17_at_12.53.42_ceemyl.png',
+    imagePublicId: 'course/Screenshot_2024-06-17_at_12.53.42_ceemyl',
+    modules: [
+      {
+        name: 'Video Editing',
+        description:
+          'Learn the basics of video editing from cutting clips to adding effects.',
+        lessons: [
+          {
+            name: 'Creating a composition for video shooting',
+            description:
+              'Peculiarities of camera placement in different situations',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718613908/lesson/oqsgcdxxtfxwsumdvmdt.mp4',
+            videoPublicId: 'lesson/oqsgcdxxtfxwsumdvmdt',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Advanced Mathematics',
+    description: 'Explore complex mathematical theories and applications.',
+    category: 'academic-subjects',
+    subcategory: 'mathematics-and-statistics',
+    price: 15,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718614669/course/Screenshot_2024-06-17_at_12.54.34_ilmhfz.png',
+    imagePublicUrl: 'course/Screenshot_2024-06-17_at_12.54.34_ilmhfz',
+    modules: [
+      {
+        name: 'Calculus',
+        description:
+          'Dive into the fundamentals of calculus, including derivatives and integrals.',
+        lessons: [
+          {
+            name: 'Fundamentals',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718613937/lesson/v5xyywkodakcoi5jkhnz.mp4',
+            videoPublicId: 'lesson/v5xyywkodakcoi5jkhnz',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Gardening',
+    description: 'Learn about plant care and landscape management.',
+    category: 'lifestyle-and-entertainment',
+    subcategory: 'gardening-and-horticulture',
+    price: 10,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718614682/course/Screenshot_2024-06-17_at_12.56.27_n09tn9.png',
+    imagePublicId: 'course/Screenshot_2024-06-17_at_12.56.27_n09tn9',
+    modules: [
+      {
+        name: 'Plant Basics',
+        description: 'Get to know the essentials of plant care from an expert.',
+        lessons: [
+          {
+            name: 'Spikes',
+            description: 'Learn more about spikes',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718614007/lesson/ucwzryxa9agf3vbgclgp.mp4',
+            videoPublicId: 'lesson/ucwzryxa9agf3vbgclgp',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Oil Painting',
+    description: 'Master the art of painting with oil-based paints.',
+    category: 'arts-design-and-media',
+    subcategory: 'illustrations',
+    price: 20,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dbrquscbv/image/upload/v1718614703/course/Screenshot_2024-06-17_at_12.58.00_nmugaa.png',
+    imagePublicId: 'course/Screenshot_2024-06-17_at_12.58.00_nmugaa',
+    modules: [
+      {
+        name: 'Painting Techniques',
+        description:
+          'Learn various oil painting techniques to enhance your artistic skills.',
+        lessons: [
+          {
+            name: 'Brush Handling',
+            description: 'Gain control and finesse in your brushwork.',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718614192/lesson/ew5rs2npwmwrnnk5eqtx.mp4',
+            videoPublicId: 'lesson/ew5rs2npwmwrnnk5eqtx',
+          },
+        ],
+      },
+      {
+        name: 'The first picture',
+        description: 'Create your first painting',
+        imageUrl:
+          'https://res.cloudinary.com/dbrquscbv/image/upload/v1718614671/course/Screenshot_2024-06-17_at_12.56.59_cn0gkc.png',
+        imagePublicId: 'course/Screenshot_2024-06-17_at_12.56.59_cn0gkc',
+        lessons: [
+          {
+            name: 'Landscape',
+            videoUrl:
+              'https://res.cloudinary.com/dbrquscbv/video/upload/v1718614152/lesson/duuy927rxzcxwenkhew0.mp4',
+            videoPublicId: 'lesson/duuy927rxzcxwenkhew0',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const coursedDataSnezhannaProd = [
+  {
+    name: 'Videography',
+    description:
+      'Courses on shooting and editing video to create stunning visual content.',
+    category: 'arts-design-and-media',
+    subcategory: 'video-production-and-editing',
+    price: 25,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212243/Screenshot_2024-06-17_at_12.53.42_ceemyl_hyt0mo.png',
+    imagePublicId: 'Screenshot_2024-06-17_at_12.53.42_ceemyl_hyt0mo',
+    modules: [
+      {
+        name: 'Video Editing',
+        description:
+          'Learn the basics of video editing from cutting clips to adding effects.',
+        lessons: [
+          {
+            name: 'Creating a composition for video shooting',
+            description:
+              'Peculiarities of camera placement in different situations',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212365/oqsgcdxxtfxwsumdvmdt_skxvyw.mp4',
+            videoPublicId: 'oqsgcdxxtfxwsumdvmdt_skxvyw',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Advanced Mathematics',
+    description: 'Explore complex mathematical theories and applications.',
+    category: 'academic-subjects',
+    subcategory: 'mathematics-and-statistics',
+    price: 15,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212245/Screenshot_2024-06-17_at_12.54.34_ilmhfz_scbh3m.png',
+    imagePublicUrl: 'Screenshot_2024-06-17_at_12.54.34_ilmhfz_scbh3m',
+    modules: [
+      {
+        name: 'Calculus',
+        description:
+          'Dive into the fundamentals of calculus, including derivatives and integrals.',
+        lessons: [
+          {
+            name: 'Fundamentals',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212392/v5xyywkodakcoi5jkhnz_qfp8gu.mp4',
+            videoPublicId: 'v5xyywkodakcoi5jkhnz_qfp8gu',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Gardening',
+    description: 'Learn about plant care and landscape management.',
+    category: 'lifestyle-and-entertainment',
+    subcategory: 'gardening-and-horticulture',
+    price: 10,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212261/Screenshot_2024-06-17_at_12.56.27_n09tn9_msvxkn.png',
+    imagePublicId: 'Screenshot_2024-06-17_at_12.56.27_n09tn9_msvxkn',
+    modules: [
+      {
+        name: 'Plant Basics',
+        description: 'Get to know the essentials of plant care from an expert.',
+        lessons: [
+          {
+            name: 'Spikes',
+            description: 'Learn more about spikes',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212381/ucwzryxa9agf3vbgclgp_lpwgar.mp4',
+            videoPublicId: 'ucwzryxa9agf3vbgclgp_lpwgar',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Oil Painting',
+    description: 'Master the art of painting with oil-based paints.',
+    category: 'arts-design-and-media',
+    subcategory: 'illustrations',
+    price: 20,
+    currency: 'TON',
+    imageUrl:
+      'https://res.cloudinary.com/dkqwhqrmh/image/upload/v1719212260/Screenshot_2024-06-17_at_12.58.00_nmugaa_venw6k.png',
+    imagePublicId: 'Screenshot_2024-06-17_at_12.58.00_nmugaa_venw6k',
+    modules: [
+      {
+        name: 'Painting Techniques',
+        description:
+          'Learn various oil painting techniques to enhance your artistic skills.',
+        lessons: [
+          {
+            name: 'Brush Handling',
+            description: 'Gain control and finesse in your brushwork.',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212367/ew5rs2npwmwrnnk5eqtx_uwpcrl.mp4',
+            videoPublicId: 'ew5rs2npwmwrnnk5eqtx_uwpcrl',
+          },
+        ],
+      },
+      {
+        name: 'The first picture',
+        description: 'Create your first painting',
+        imageUrl:
+          'https://res.cloudinary.com/dbrquscbv/image/upload/v1718614671/course/Screenshot_2024-06-17_at_12.56.59_cn0gkc.png',
+        imagePublicId: 'course/Screenshot_2024-06-17_at_12.56.59_cn0gkc',
+        lessons: [
+          {
+            name: 'Landscape',
+            videoUrl:
+              'https://res.cloudinary.com/dkqwhqrmh/video/upload/v1719212364/duuy927rxzcxwenkhew0_tz4ag7.mp4',
+            videoPublicId: 'duuy927rxzcxwenkhew0_tz4ag7',
+          },
+        ],
+      },
+    ],
+  },
+];
